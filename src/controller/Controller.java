6303dbd513b38d1a14cb1b76f645aba43208f695
Vladimir Lazic <vladimir12934@gmail.com>
2020-02-29 23:07:50 +0100

    Few more changes at FDriver (load some tables in sql etc).

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,6 +3,7 @@ package controller;
 import db.DBBroker;
 import domain.Driver;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -36,13 +37,39 @@ public class Controller {
     }
 
     public void saveDriver(Driver d) throws ClassNotFoundException, SQLException {
-        String query = "create table driver(idcard numeric(10), name varchar(25), surname varchar(25));";
+        String name = d.getName();
+        String surname = d.getSurname();
+        int IDCard = d.getIDCard();
+        String query = "insert into driver values" + "(" + " ' " +IDCard + " ' " + ","+ " ' " + name + " ' " + "," + " ' " + surname + " ' " +  ");";
+        System.out.println("insert into driver values" + "(" + " ' " +IDCard + " ' " + ","+ " ' " + name + " ' " + "," + " ' " + surname + " ' " +  ");");
         db.loadDriver();
         connection = db.openConnection();
         System.out.println("SUCCESS");
         Statement s = connection.createStatement();
         s.executeUpdate(query);
         db.closeConnection();
+        
+    
+        
+    }
+
+    public void loginFullBase() throws SQLException, ClassNotFoundException {
+        String query = "create table driver(idcard numeric(10), name varchar(25), surname varchar(25));";
+        db.loadDriver();
+        connection = db.openConnection();
+        
+        DatabaseMetaData dbm = connection.getMetaData();
+
+        ResultSet tables = dbm.getTables(null, null, "driver", null);
+        if (tables.next()) {
+            db.closeConnection();
+        }
+        else {
+            System.out.println("SUCCESS");
+         Statement s = connection.createStatement();
+        s.executeUpdate(query);
+        db.closeConnection();
+        }
     }
     
     
